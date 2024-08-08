81859216488b748ef35f430fc12c694d0a260fcf
Rodrigo Viana <digo1209@gmail.com>
2024-08-08 15:34:31 -0300

    Finalizando as seções de parceiros e whatGPT-3

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,7 +8,7 @@ const Menu = () => {
   return(
     <>
     <p><a href="#home">Home</a></p>
-    <p><a href="#gpt3">What is GPT3?</a></p>
+    <p><a href="#wgpt3">What is GPT3?</a></p>
     <p><a href="#possibility">HomeOpen AI</a></p>
     <p><a href="#features">Case Studies</a></p>
     <p><a href="#blog">Library</a></p>
