82993a6236164b5c4826d23b83a8650531f69a56
Rodrigo Viana <digo1209@gmail.com>
2024-08-08 13:53:43 -0300

    Header finalizado

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -19,9 +19,9 @@ const Header = () => {
             <p>1.600 requested access a visit in last 24 hours</p>
           </div>
 
-          <div className="gpt3__header-image">
-            <img src={ai} alt="ai" />
-          </div>
+        </div>
+        <div className="gpt3__header-image">
+          <img src={ai} alt="ai" />
         </div>
     </div>
   )
