141d4c2f81f89f3955f95564872b5d925e7de25e
Rodrigo Viana <digo1209@gmail.com>
2024-08-09 16:21:47 -0300

    Projeto finalizado

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import './blog.css'
+import { Article } from '../../components'
+import {blog1, blog2, blog3, blog4, blog5} from './imports'
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div className='gpt3__blog section__padding' id='blog'>
+      <div className="gpt3__blog-heading">
+        <h1 className="gradient__text">A lot is happening. We are blogging about it</h1>
+      </div>
+      <div className="gpt3__blog-container">
+        <div className="gpt3__blog-container_groupA">
+          <Article imgUrl={blog1} date="Data pra colocar" title="Titulo a ser mudado. Colocando mais umas coisas que o titulo é maior"/>
+        </div>
+        <div className="gpt3__blog-container_groupB">
+          <Article imgUrl={blog2} date="Data pra colocar" title="Titulo a ser mudado. Colocando mais umas coisas que o titulo é maior"/>
+          <Article imgUrl={blog3} date="Data pra colocar" title="Titulo a ser mudado. Colocando mais umas coisas que o titulo é maior"/>
+          <Article imgUrl={blog4} date="Data pra colocar" title="Titulo a ser mudado. Colocando mais umas coisas que o titulo é maior"/>
+          <Article imgUrl={blog5} date="Data pra colocar" title="Titulo a ser mudado. Colocando mais umas coisas que o titulo é maior"/>
+        </div>
+      </div>
+    </div>
   )
 }
 
